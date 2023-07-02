526230f2566b2146d507f2697fca1ebe38e10fab
ivanjermakov <ivanjermakov1@gmail.com>
2023-07-02 04:01:38 +0200

    Basic editor, parseTree & error preview

--- src/component/app/App.tsx
+++ src/component/app/App.tsx
@@ -1,11 +1,101 @@
 import type { Component } from 'solid-js'
+import { createEffect, createSignal, For, JSX, Match, Switch } from 'solid-js'
 
 import styles from './App.module.sass'
+import { ParseNode } from 'nois/dist/parser'
+import { erroneousTokenKinds, ParseToken, tokenize } from 'nois/dist/lexer/lexer'
+import { prettyLexerError, prettySourceMessage, prettySyntaxError, SyntaxError } from 'nois/dist/error'
+import { indexToLocation } from 'nois/dist/location'
+import { Parser } from 'nois/dist/parser/parser'
+import { parseModule } from 'nois/dist/parser/fns'
+import { buildModuleAst, Module } from 'nois/dist/ast'
+import { useColoredOutput } from 'nois/dist/output'
 
 const App: Component = () => {
+    const defaultCode = `\
+fn main() {
+    println("Hello, World!")
+}`
+    const [code, setCode] = createSignal(defaultCode)
+    const source = () => ({ str: code(), filename: 'test.no' })
+    const vid = { scope: [], name: 'test' }
+    const [module, setModule] = createSignal<Module>()
+    const [errorTokens, setErrorTokens] = createSignal<ParseToken[]>()
+    const [syntaxErrors, setSyntaxErrors] = createSignal<SyntaxError[]>()
+
+    createEffect(() => {
+        useColoredOutput(false)
+        const tokens = tokenize(source().str)
+        const errorTs = tokens.filter(t => erroneousTokenKinds.includes(t.kind))
+        if (errorTs.length > 0) {
+            setErrorTokens(errorTs)
+            setModule(undefined)
+            setSyntaxErrors(undefined)
+            return
+        }
+
+        const parser = new Parser(tokens)
+        parseModule(parser)
+        const root = parser.buildTree()
+
+        if (parser.errors.length > 0) {
+            setSyntaxErrors(parser.errors)
+            setErrorTokens(undefined)
+            setModule(undefined)
+            return
+        }
+
+        setModule(buildModuleAst(root, vid))
+        setErrorTokens(undefined)
+        setSyntaxErrors(undefined)
+    })
+
+    const toHtml = (node: ParseNode): JSX.Element => {
+        return <div class={styles.parseNode}>
+            <p class={styles.kind}>{node.kind}{'value' in node ? <span>: {node.value}</span> : ''}</p>
+            <div class={styles.child}>
+                {'nodes' in node ? node.nodes.map(toHtml) : ''}
+            </div>
+        </div>
+    }
     return (
         <div class={styles.App}>
-            <p>Hello</p>
+            <div class={styles.editor}>
+                <textarea value={code()} onkeyup={e => setCode((e.target as HTMLTextAreaElement).value)}/>
+            </div>
+            <div class={styles.rightPanel}>
+                <Switch>
+                    <Match when={module()}>
+                        <div class={styles.parseTreeViewer}>{toHtml(module()!.parseNode)}</div>
+                    </Match>
+                    <Match when={errorTokens()}>
+                        <div class={styles.lexerErrorViewer}>
+                            <For each={errorTokens()}>{t =>
+                                <pre>
+                                {prettySourceMessage(
+                                    prettyLexerError(t),
+                                    indexToLocation(t.location.start, source())!,
+                                    source()
+                                )}
+                                </pre>}
+                            </For>
+                        </div>
+                    </Match>
+                    <Match when={syntaxErrors()}>
+                        <div class={styles.syntaxErrorViewer}>
+                            <For each={syntaxErrors()}>{e =>
+                                <pre>
+                                {prettySourceMessage(
+                                    prettySyntaxError(e),
+                                    indexToLocation(e.got.location.start, source())!,
+                                    source()
+                                )}
+                            </pre>
+                            }</For>
+                        </div>
+                    </Match>
+                </Switch>
+            </div>
         </div>
     )
 }
--- src/index.tsx
+++ src/index.tsx
@@ -4,10 +4,4 @@ import { render } from 'solid-js/web'
 import './index.sass'
 import App from './component/app/App'
 
-const root = document.getElementById('root')
-
-if (import.meta.env.DEV && !(root instanceof HTMLElement)) {
-    throw new Error('no root element',)
-}
-
-render(() => <App/>, root!)
+render(() => <App/>, (document.getElementById('root'))!)
