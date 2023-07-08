e7b718a404ca3b6c163e7d2fa55a5cf6269690bd
ivanjermakov <ivanjermakov1@gmail.com>
2023-07-08 23:34:20 +0200

    Fix type complaint

--- src/component/toolbar/Toolbar.tsx
+++ src/component/toolbar/Toolbar.tsx
@@ -1,7 +1,7 @@
-import { Component, JSX } from 'solid-js'
+import { Component, ParentProps } from 'solid-js'
 import styles from './Toolbar.module.sass'
 
-export const Toolbar: Component = (p): JSX.Element => {
+export const Toolbar: Component<ParentProps> = p => {
     return (
         <div class={styles.Toolbar}>
             {p.children}
