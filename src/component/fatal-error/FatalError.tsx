60a84fca83fbb7084e853c62597a634aed3bde30
ivanjermakov <ivanjermakov1@gmail.com>
2023-07-08 23:30:55 +0200

    Copy button tooltip

--- src/component/fatal-error/FatalError.tsx
+++ src/component/fatal-error/FatalError.tsx
@@ -1,19 +1,34 @@
 import { Component } from 'solid-js'
 import styles from './FatalError.module.sass'
 import { Toolbar } from '../toolbar/Toolbar'
+import tippy from 'tippy.js'
+import 'tippy.js/dist/tippy.css'
 
 type FatalErrorProps = { message: string }
 
 export const FatalError: Component<FatalErrorProps> = p => {
+    const copyToClipboard = async () => {
+        await navigator.clipboard.writeText(p.message)
+        tippy(copyButton!, {
+            content: 'copied!',
+            showOnCreate: true,
+            theme: 'nois',
+            onHidden: t => t.destroy(),
+            delay: [0, 200]
+        })
+    }
+
+    let copyButton: HTMLButtonElement | undefined
     return <div class={styles.FatalError}>
         <p>Congratulations! You broke the compiler. 😑</p>
         <p><a href={'https://github.com/nois-lang/nois/issues'}>Report</a> the message below:</p>
         <div class={styles.container}>
             <Toolbar>
                 <button
+                    ref={copyButton}
                     type={'button'}
                     title={'Copy message to clipboard'}
-                    onClick={() => copyToClipboard(p.message)}
+                    onClick={copyToClipboard}
                 >
                     <i class="fa-solid fa-clipboard"/>
                 </button>
@@ -23,6 +38,3 @@ export const FatalError: Component<FatalErrorProps> = p => {
     </div>
 }
 
-const copyToClipboard = (msg: string) => {
-    navigator.clipboard.writeText(msg)
-}
