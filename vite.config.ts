01c0bd550e8ec90b27a94afa9733e001930e90de
ivanjermakov <ivanjermakov1@gmail.com>
2024-01-15 14:37:47 +0100

    Copy `std` to `public`

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,20 @@
 import { defineConfig } from 'vite'
 import solidPlugin from 'vite-plugin-solid'
 import monacoEditorPlugin from 'vite-plugin-monaco-editor'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 export default defineConfig({
     plugins: [
         solidPlugin(),
-        (<any>monacoEditorPlugin).default({ languageWorkers: ['editorWorkerService'] })
+        (<any>monacoEditorPlugin).default({
+            languageWorkers: ['editorWorkerService']
+        }),
+        viteStaticCopy({
+            targets: [{ src: 'node_modules/nois/dist/std', dest: 'public' }]
+        })
     ],
     server: {
-        port: 3000,
+        port: 3000
     },
     build: {
         target: 'esnext',
@@ -17,5 +23,5 @@ export default defineConfig({
                 manualChunks: { 'monaco-editor': ['monaco-editor'] }
             }
         }
-    },
+    }
 })
