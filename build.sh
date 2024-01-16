dc3de7b63410509f9e8d534eb231dda99f93134c
ivanjermakov <ivanjermakov1@gmail.com>
2024-01-16 21:01:55 +0100

    Build script

---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+git clone https://github.com/nois-lang/nois ../nois
+pushd .
+cd ../nois
+npm install
+npm run build
+ls dist
+
+popd
+rustup default stable
+cargo install mdbook
+npm install
+npm run build
+ls dist
