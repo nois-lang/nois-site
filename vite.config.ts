33f7c42b41eb18d5094503f50094b2a61ce1f351
ivanjermakov <ivanjermakov1@gmail.com>
2024-03-14 18:09:08 +0100

    Don't mangle nois stack trace

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,14 @@
+import terser from '@rollup/plugin-terser'
+import { RenderedChunk } from 'rollup'
+import { MinifyOptions } from 'terser'
 import { defineConfig } from 'vite'
 import solidPlugin from 'vite-plugin-solid'
 import { viteStaticCopy } from 'vite-plugin-static-copy'
+
+const chunkMap = [
+    { pattern: 'nois/dist/', dest: 'nois', mangle: false },
+    { pattern: '@codemirror', dest: 'codemirror', mangle: true }
+]
 
 export default defineConfig({
     plugins: [
@@ -13,6 +21,28 @@ export default defineConfig({
         port: 3000
     },
     build: {
-        target: 'esnext'
+        target: 'esnext',
+        rollupOptions: {
+            plugins: [
+                {
+                    name: 'terser',
+                    renderChunk: (code: string, chunk: RenderedChunk, outputOptions: any) => {
+                        const terserOptions: MinifyOptions = { format: { comments: false } }
+                        const opts = chunkMap.find(c => c.dest === chunk.name)
+                        if (opts && !opts.mangle) {
+                            terserOptions.keep_fnames = true
+                        }
+                        return (<any>terser(terserOptions)).renderChunk(code, chunk, outputOptions)
+                    }
+                }
+            ],
+            output: {
+                manualChunks: id => {
+                    return chunkMap.find(({ pattern }) => id.includes(pattern))?.dest
+                }
+            }
+        },
+        minify: false,
+        sourcemap: true
     }
 })
